6c5a83b5c058224d16f42106df28263df254d7eb
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-22 11:46:29 +0800

    I deleted my pseudocode since my algorithm or rather my source code is leaning as of now into experimental (where I brainstorm ideas and do a run-testing). I use a for loop together with the .items() to toggle around the datas inside the dictionary; where I called the names(s) and the details within each entry (with a header of their name) since a dictionary is a key-values pairs that's why there's two variables being addressed in the dictionary using for loop.

--- entries_for_name_and_age_and_after_that_displays_the_oldest_person.py
+++ entries_for_name_and_age_and_after_that_displays_the_oldest_person.py
@@ -1,11 +1,5 @@
-# Make a var for an array of dictionary {} containing the entry/entries of the user/s (name/s and their age; if more than 1 entry); applying max function to the dictionary to determine the oldest person
 entry_or_entries_of_the_user_or_users = {}
 
-# A while loop that evaluates the user/s if he/she/they wants to input another entry and if not, the loop will be satisfied and be finished (the basis for the evaluation is given by the user/s inside the another while loop which is inside of this while loop); this loop goes from the 2nd Loop up to the evaluation 
-    # A while loop again inside the previous while loop (1st Loop); a loop where it raises an error whenever the user/s entered an invalid input; and this loop goes from inputting the necessary datas, storing it to the our dedicated dictionary, printing the desired data, and lastly, verifying if the user/s want to input another entry
-        # Error function: try (run the given algoruthm)
-            # name input
-            # age input  
 allowed_special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/" 
 
 while True:    
@@ -29,8 +23,8 @@ while True:
             "Age" : entry_age
         }
 
-        print(entry_or_entries_of_the_user_or_users[entry_name]["Name"])
-        print(entry_or_entries_of_the_user_or_users[entry_name]["Age"])
+        print(f'Name: {entry_or_entries_of_the_user_or_users[entry_name]["Name"]}')
+        print(f'Age: {entry_or_entries_of_the_user_or_users[entry_name]["Age"]}')
  
         while True:
             another_entry = input("Would you like to submit another entry? (Yes/No): ").capitalize()      
@@ -45,16 +39,6 @@ while True:
         if another_entry == "No":
             break       
             
-            # the_assigned_dictionary[name] = {"age" : age}; storing the inputted datas into the dictionary
-
-            # print the oldest person (using the dictionary with the max function)
-
-            # Asks the user/s if he/she/they wants to input another entry 
-            # break
-        # Error function: except (raises the error)
-
-    # if user_will_input_another_entry is == to the inputted string "No"
-        # then print the oldest person (using the dictionary with the max function)
-        # break
-    # elif user_will_input_another_entry is != to "Yes"
-        # then print "Invalid input, please try again."
+for name_entry_or_entries, age_entry_or_entries in entry_or_entries_of_the_user_or_users.items():
+    print(f"{name_entry_or_entries}: {age_entry_or_entries}")     
+            
