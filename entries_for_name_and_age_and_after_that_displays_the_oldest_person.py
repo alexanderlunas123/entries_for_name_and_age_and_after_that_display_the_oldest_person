33b3745cca4105f2e7c959136eb7985187f60c80
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-10-22 10:52:07 +0800

    I modified the algorithm part for the input about submitting another entry or not, my previous algorithm just keeps looping back to the submitting another entry input (I actually do some experiments after the previous commit source code, and this is the best and successful tweaking on the source code).

--- entries_for_name_and_age_and_after_that_displays_the_oldest_person.py
+++ entries_for_name_and_age_and_after_that_displays_the_oldest_person.py
@@ -6,40 +6,45 @@ entry_or_entries_of_the_user_or_users = {}
         # Error function: try (run the given algoruthm)
             # name input
             # age input  
-allowed_special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/"            
+allowed_special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/" 
 
 while True:    
- while True:
-    entry_name = input("Enter the name (can be you or anyone): ").strip()
-    replacing_spaced_names = entry_name.replace(" ", "")
-    if all(char.isalnum() or char in allowed_special_characters for char in replacing_spaced_names):
-        break   
-    else:
-        print("The inputted character(s) is/are invalid.")  
-        print("Please try again")
- while True:          
-    try:             
-        entry_age = int(input("Enter the age (your age or their age): "))
-        break  
-    except:
-        print("Invalid input, must be a number.")   
+        while True:
+            entry_name = input("Enter the name (can be you or anyone): ").strip()
+            replacing_spaced_names = entry_name.replace(" ", "")
+            if all(char.isalnum() or char in allowed_special_characters for char in replacing_spaced_names):
+                break   
+            else:
+                print("The inputted character(s) is/are invalid.")  
+                print("Please try again")
+        while True:          
+            try:             
+                entry_age = int(input("Enter the age (your age or their age): "))
+                break  
+            except:
+                print("Invalid input, must be a number.")   
 
- entry_or_entries_of_the_user_or_users[entry_name] = { 
-    "Name" : entry_name,
-    "Age" : entry_age
- }
+        entry_or_entries_of_the_user_or_users[entry_name] = { 
+            "Name" : entry_name,
+            "Age" : entry_age
+        }
 
- print(entry_or_entries_of_the_user_or_users[entry_name]["Name"])
- print(entry_or_entries_of_the_user_or_users[entry_name]["Age"])
+        print(entry_or_entries_of_the_user_or_users[entry_name]["Name"])
+        print(entry_or_entries_of_the_user_or_users[entry_name]["Age"])
  
- while True:
-    another_entry = input("Would you like to submit another entry? (Yes/No): ")
-    another_entry = another_entry.capitalize()
-    if another_entry.isalpha():
-        break
-    else:
-        print("Invalid input, 'Yes' or 'No' only.")
-        print("Please try again.")      
+        while True:
+            another_entry = input("Would you like to submit another entry? (Yes/No): ").capitalize()      
+            if another_entry == "No":
+                break
+            elif another_entry == "Yes":
+                break
+            else: 
+                print("Invalid input, 'Yes' or 'No' only.")
+                print("Please try again.")
+
+        if another_entry == "No":
+            break       
+            
             # the_assigned_dictionary[name] = {"age" : age}; storing the inputted datas into the dictionary
 
             # print the oldest person (using the dictionary with the max function)
